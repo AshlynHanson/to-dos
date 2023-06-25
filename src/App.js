8b2f154a65b6c7f34c8867ea87967a393532e68c
Ashlyn Hanson <adh5584@truman.edu>
2023-06-25 16:15:41 -0700

    set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo title='Learn React'/>
+      <Todo title='Explore React'/>
+      <Todo title='Use React'/>
+    </div>
+  );
+}
+
+export default App;
